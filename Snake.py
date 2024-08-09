a667e76af79b34cd52a81bc35818e5a5616f139d
abdullahjanjuas <114680801+abdullahjanjuas@users.noreply.github.com>
2024-08-09 12:55:46 -0700

    Update Snake.py

--- Snake.py
+++ Snake.py
@@ -1,29 +1,63 @@
 from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0  
 
-ALIGNMENT = "center"
-FONT = ("Arial",24,"normal")
-
-class Scoreboard(Turtle):
+class Snake:
+    
+    #Constrctor
     def __init__(self):
-        super().__init__()
-        self.score = 0
-        self.high_score = 0
-        self.color("white")
-        self.penup()
-        self.goto(0,260)
-        self.hideturtle()
-        self.update_scoreboard()
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+     
+    #Creates the snake body    
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
+            
+    def extend(self):
+            self.add_segment(self.segments[-1].position())
+           
+    def add_segment(self,position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
         
-    def update_scoreboard(self):
-        self.clear()
-        self.write(f"Score:{self.score} High Score:{self.high_score}", align = ALIGNMENT, font = FONT )
+    def reset_snake(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
         
-    def reset_score(self):
-        if self.score > self.high_score:
-            self.high_score = self.score
-        self.score = 0
-        self.update_scoreboard()
+            
+    #Moves the snake body
+    def move(self):
+            for seg_num in range(len(self.segments)-1, 0, -1):
+                new_x = self.segments[seg_num-1].xcor()
+                new_y = self.segments[seg_num-1].ycor()
+                self.segments[seg_num].goto(new_x,new_y)
+            self.head.forward(MOVE_DISTANCE)
+    
+    #Control snake body using keystrokes
+    def up(self):
+        if self.head.heading() != DOWN:
+             self.head.setheading(UP)
         
-    def increase_score(self):
-        self.score += 1
-        self.update_scoreboard()
+    def down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+        
+    def right(self):
+        if self.head.heading() != LEFT:
+          self.head.setheading(RIGHT)
+        
+    def left(self):
+        if self.head.heading() != RIGHT:
+          self.head.setheading(LEFT)
